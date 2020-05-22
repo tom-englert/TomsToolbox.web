00fb9d26ce3c40669cc986da9fc890444452dde1
tom-englert <mail@tom-englert.de>
2020-05-22 16:56:40 +0200

    Use material design

--- ng-workspace/src/app/app.component.spec.ts
+++ ng-workspace/src/app/app.component.spec.ts
@@ -1,13 +1,30 @@
 import {TestBed, async} from '@angular/core/testing';
 import {AppComponent} from './app.component';
 import {AngularModule, CultureService, IntlAdapterService} from "angular";
+import {BrowserModule} from "@angular/platform-browser";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FormsModule} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatRippleModule} from "@angular/material/core";
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
-      ]
+      ],
+      imports: [
+        AngularModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatRippleModule,
+      ],
     }).compileComponents();
   }));
 
--- ng-workspace/src/app/app.module.ts
+++ ng-workspace/src/app/app.module.ts
@@ -3,14 +3,26 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {AngularModule, CultureService, IntlAdapterService} from "angular";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {FormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatRippleModule} from "@angular/material/core";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AngularModule,
     BrowserModule,
-    AngularModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
   ],
   providers: [
     IntlAdapterService,
