4911533e867f61713c0aa834323c3ef2351434b2
tom-englert <mail@tom-englert.de>
2020-07-07 15:35:50 +0200

    add missing FormatDatePipe to public-api

--- ng-workspace/projects/angular/src/public-api.ts
+++ ng-workspace/projects/angular/src/public-api.ts
@@ -2,7 +2,8 @@
  * Public API Surface of angular
  */
 
-export * from './lib/toms-angular.module';
 export * from './lib/culture-service'
 export * from './lib/custom-date-adapter'
+export * from './lib/format-date.pipe'
 export * from './lib/intl-adapter.service'
+export * from './lib/toms-angular.module';
