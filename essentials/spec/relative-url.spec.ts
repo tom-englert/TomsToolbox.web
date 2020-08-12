d6e42677941dd97bc1759d721df9b4b76d5a2c4f
tom-englert <mail@tom-englert.de>
2020-08-12 09:46:09 +0200

    relative-url implements full URL interface and supports modifications.

--- essentials/spec/relative-url.spec.ts
+++ essentials/spec/relative-url.spec.ts
@@ -26,6 +26,16 @@ describe('RelativeURL', () => {
         expect(target.search).toBe('?nix=alles');
     });
 
+    it('should parse multiple segments with search and hash', () => {
+        const target = new RelativeUrl('a/b/c?nix=alles#theHash');
+        expect(target.searchParams.has('nix')).toBe(true);
+        expect(target.searchParams.get('nix')).toBe('alles');
+        expect(target.pathSegments).toEqual(['a', 'b', 'c']);
+        expect(target.path).toBe('a/b/c');
+        expect(target.search).toBe('?nix=alles');
+        expect(target.hash).toBe('#theHash')
+    });
+
     it('parses single segment', () => {
         const target = new RelativeUrl('a');
         expect(target.searchParams.has('nix')).toBe(false);
@@ -58,4 +68,101 @@ describe('RelativeURL', () => {
         expect(target.path).toBe('a');
         expect(target.search).toBe('?nix=alles');
     });
+
+    it('composes original source', () => {
+        const source = 'host/a/b/c?p1=1&p2=2';
+        const url = new RelativeUrl(source);
+        expect(url.searchParams.toString()).toBe('p1=1&p2=2');
+        expect(url.toString()).toBe(source);
+    })
+
+    it('composes original source', () => {
+        const source = 'a/b/c?p1=1&p2=2#hash';
+        const url = new RelativeUrl(source);
+        expect(url.searchParams.toString()).toBe('p1=1&p2=2');
+        expect(url.toString()).toBe(source);
+    })
+
+    it('escapes special characters', () => {
+        const source = '"a"/b/c?p1=1&p2=2#hash';
+        const url = new RelativeUrl(source);
+        expect(url.searchParams.toString()).toBe('p1=1&p2=2');
+        expect(url.toString()).toBe('%22a%22/b/c?p1=1&p2=2#hash');
+    })
+
+    it('decomposes hash with special characters', () => {
+        const source = '"a"/b/c?p1=1&p2=2#hash?etc&more';
+        const url = new RelativeUrl(source);
+        expect(url.searchParams.toString()).toBe('p1=1&p2=2');
+        expect(url.hash).toBe('#hash?etc&more');
+        expect(url.toString()).toBe('%22a%22/b/c?p1=1&p2=2#hash?etc&more');
+    })
+
+    it('handles parameter replacement', () => {
+        const source = 'a/b/c?p1=1&p2=2#hash';
+        const url = new RelativeUrl(source);
+        url.searchParams.delete('p1');
+        url.searchParams.append('p3', 'test')
+        expect(url.toString()).toBe('a/b/c?p2=2&p3=test#hash');
+    })
+
+    it('handles pathname replacement', () => {
+        const source = 'a/b/c?p1=1&p2=2#hash';
+        const url = new RelativeUrl(source);
+        url.pathname = 'd/e';
+        expect(url.toString()).toBe('d/e?p1=1&p2=2#hash');
+    })
+
+    it('handles pathname replacement with preceding slash', () => {
+        const source = 'a/b/c?p1=1&p2=2#hash';
+        const url = new RelativeUrl(source);
+        url.pathname = '/d/e';
+        expect(url.toString()).toBe('d/e?p1=1&p2=2#hash');
+    })
+
+    it('returns json with same as source', () => {
+        const source = 'a/b/c?p1=1&p2=2#hash';
+        const url = new RelativeUrl(source);
+        expect(url.toJSON()).toBe(source);
+    })
+});
+
+describe('original URL implementation', () => {
+    it('composes original source', () => {
+        const source = 'http://host/a/b/c?p1=1&p2=2';
+        const url = new URL(source);
+        expect(url.searchParams.toString()).toBe('p1=1&p2=2');
+        expect(url.toString()).toBe(source);
+    })
+    it('composes original source', () => {
+        const source = 'http://host/a/b/c?p1=1&p2=2#hash';
+        const url = new URL(source);
+        expect(url.searchParams.toString()).toBe('p1=1&p2=2');
+        expect(url.toString()).toBe(source);
+    })
+    it('escapes special characters', () => {
+        const source = 'http://host/"a"/b/c?p1=1&p2=2#hash';
+        const url = new URL(source);
+        expect(url.searchParams.toString()).toBe('p1=1&p2=2');
+        expect(url.toString()).toBe('http://host/%22a%22/b/c?p1=1&p2=2#hash');
+    })
+    it('decomposes hash with special characters', () => {
+        const source = 'http://host/"a"/b/c?p1=1&p2=2#hash?etc&more';
+        const url = new URL(source);
+        expect(url.searchParams.toString()).toBe('p1=1&p2=2');
+        expect(url.hash).toBe('#hash?etc&more');
+        expect(url.toString()).toBe('http://host/%22a%22/b/c?p1=1&p2=2#hash?etc&more');
+    })
+    it('handles parameter replacement', () => {
+        const source = 'http://host/a/b/c?p1=1&p2=2#hash';
+        const url = new URL(source);
+        url.searchParams.delete('p1');
+        url.searchParams.append('p3', 'test')
+        expect(url.toString()).toBe('http://host/a/b/c?p2=2&p3=test#hash');
+    })
+    it('returns json with same as source', () => {
+        const source = 'http://host/a/b/c?p1=1&p2=2#hash';
+        const url = new URL(source);
+        expect(url.toJSON()).toBe(source);
+    })
 });
--- essentials/src/relative-url.ts
+++ essentials/src/relative-url.ts
@@ -1,34 +1,96 @@
 /**
  * A helper class to properly split a relative url into its parts
  */
-export class RelativeUrl {
+export class RelativeUrl implements URL {
+
+    private url: URL;
+
+    get hash() {
+        return this.url.hash;
+    }
+
+    set hash(value) {
+        this.url.hash = value;
+    }
+
+    get pathname() {
+        return this.url.pathname.substr(1);
+    }
+
+    set pathname(value) {
+        this.url.pathname = '/' + value;
+    }
 
     /**
-     * The full path of the url
+     * @deprecated only for backward compatibility, use the pathname property.
      */
-    readonly path: string;
-    /**
-     * The segments of the path
-     */
-    readonly pathSegments: string[];
-    /**
-     * The search part of the url
-     */
-    readonly search: string;
-    /**
-     * The search params of the url
-     */
-    readonly searchParams: URLSearchParams;
+    get path() {
+        return this.pathname;
+    }
+
+    get pathSegments(): string[] {
+        return this.pathname.split('/')
+    }
+
+    get search() {
+        return this.url.search;
+    }
+
+    set search(value) {
+        this.url.search = value;
+    }
+
+    get searchParams() {
+        return this.url.searchParams;
+    }
+
+    get host() {
+        return '';
+    }
+
+    get hostname() {
+        return '';
+    }
+
+    get href() {
+        return this.toString();
+    }
+
+    get origin() {
+        return '';
+    }
+
+    get password() {
+        return '';
+    }
+
+    get port() {
+        return '';
+    }
+
+    get protocol() {
+        return '';
+    }
+
+    get username() {
+        return '';
+    }
+
+    toJSON() {
+        return this.toString();
+    }
+
+    toString() : string {
+        return this.url.toString().slice(this.virtualBase.length);
+    }
+
+    private readonly virtualBase = 'file://x/';
 
     /**
      * Constructs a RelativeUrl object from a string.
      * @param relativeUrl
      */
     constructor(relativeUrl: string) {
-        const url = new URL(relativeUrl, 'file://x/');
-        this.path = url.pathname.substr(1);
-        this.pathSegments = this.path.split('/');
-        this.search = url.search;
-        this.searchParams = url.searchParams;
+        this.url = new URL(relativeUrl, this.virtualBase);
     }
 }
