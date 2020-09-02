b0088361e8cbeafaa35f7c81caa67dd302bfe133
tom-englert <mail@tom-englert.de>
2020-09-02 13:47:33 +0200

    Add missing export in public-api

--- ng-workspace/projects/angular/src/public-api.ts
+++ ng-workspace/projects/angular/src/public-api.ts
@@ -7,4 +7,5 @@ export * from './lib/custom-date-adapter'
 export * from './lib/format-date.pipe'
 export * from './lib/intl-adapter.service'
 export * from './lib/lower-case-url-serializer'
+export * from './lib/observable-property'
 export * from './lib/toms-angular.module';
