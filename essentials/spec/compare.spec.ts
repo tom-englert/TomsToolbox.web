dd4b250519b1992c058e665fee4af05c67d7b584
tom-englert <mail@tom-englert.de>
2020-09-02 13:26:13 +0200

    Add some equality comparer

---
+++ essentials/spec/compare.spec.ts
@@ -0,0 +1,51 @@
+import {isContentEqual, isEqual, isSequenceEqual, isStrictEqual} from "../src";
+
+describe('isContentEqual', () => {
+    it('should detect similar objects', () => {
+        const left = {a: 2, b: "test", c: {d: [1, 2, 3]}};
+        const right = {c: {d: [1, 2, 3]}, b: "test", a: "2"};
+
+        expect(isContentEqual(left, right)).toBeTruthy();
+    });
+    it('should detect different arrays', () => {
+        const left = {a: 2, b: "test", c: {d: [1, 2, 3]}};
+        const right = {c: {d: [1, 2, 4]}, b: "test", a: "2"};
+
+        expect(isContentEqual(left, right)).toBeFalsy();
+    });
+    it('should detect different strings', () => {
+        const left = {a: 2, b: "test", c: {d: [1, 2, 3]}};
+        const right = {c: {d: [1, 2, 3]}, b: "test1", a: "2"};
+
+        expect(isContentEqual(left, right)).toBeFalsy();
+    });
+    it('should detect similar arrays', () => {
+        const left = [1, 2, "3"];
+        const right = [1, 2, 3];
+
+        expect(isContentEqual(left, right)).toBeTruthy();
+    });
+});
+
+describe('isSequenceEqual', () => {
+    it('should detect similar arrays', () => {
+        const left = [1, 2, "3"];
+        const right = [1, 2, 3];
+
+        expect(isSequenceEqual(left, right)).toBeTruthy();
+    });
+    it('should detect different arrays', () => {
+        const left = [1, 2, "3"];
+        const right = [1, 2, 4];
+
+        expect(isSequenceEqual(left, right)).toBeFalsy();
+    });
+    it('should use the comparison method', () => {
+        const left = [1, 2, "3"];
+        const right = [1, 2, 3];
+
+        expect(isSequenceEqual(left, right)).toBeTruthy();
+        expect(isSequenceEqual(left, right, isEqual)).toBeTruthy();
+        expect(isSequenceEqual(left, right, isStrictEqual)).toBeFalsy();
+    });
+});
--- essentials/src/object/compare.ts
+++ essentials/src/object/compare.ts
@@ -3,6 +3,71 @@
  * left object precedes, follows, or occurs in the same position in the sort order as the right object.
  * Comparison is done on the native object, without any locale support,
  */
-export function compare<T>(left: T, right: T) : number {
+export function compare<T>(left: T, right: T): number {
     return (left < right ? -1 : (left > right ? 1 : 0));
 }
+
+/**
+ * A function that determines if two sequences contain the same values.
+ * @param left The left array to compare
+ * @param right The right array to compare
+ * @param comparer The comparer used to compare the individual items of the array. Default comparer is {@linkcode isContentEqual}
+ * @return `true` if both sequences are equal.
+ */
+export function isSequenceEqual<T>(left: any[], right: any[], comparer: (a: any, b: any) => boolean = isContentEqual) {
+    return Array.isArray(left) && left.every((value, index) => comparer(value, right[index]));
+}
+
+/**
+ * A function that determines if two objects have the same content.
+ * @param left The left object to compare
+ * @param right The right object to compare
+ * @return `true` if both objects are equal.
+ */
+export function isContentEqual(left: any, right: any): boolean {
+    if (left == right) {
+        return true;
+    }
+    if (!left || !right) {
+        return false;
+    }
+
+    const itemType = typeof left;
+    if (itemType != typeof right) {
+        return false;
+    }
+
+    if (itemType === 'object') {
+        if (Array.isArray(left)) {
+            return isSequenceEqual(left, right);
+        }
+
+        return Object.entries(left).every(entry => {
+            const [key, value] = entry;
+            return isContentEqual(value, right[key]);
+        });
+    }
+
+    return false;
+}
+
+/**
+ * A function that determines if two objects are equal. Used as a shortcut to the `==` operator when a comparer method is needed.
+ * @param left The left object to compare
+ * @param right The right object to compare
+ * @return `true` if both objects are equal.
+ */
+export function isEqual(left: any, right: any): boolean {
+    return (left == right);
+}
+
+/**
+ * A function that determines if two objects are equal. Used as a shortcut to the `===` operator when a comparer method is needed.
+ * @param left The left object to compare
+ * @param right The right object to compare
+ * @return `true` if both objects are equal.
+ */
+export function isStrictEqual(left: any, right: any): boolean {
+    return (left === right);
+}
+
